8c4f4988c99eb5ff02231b84e7b0c65614a165dc
ayanamesu <wlee17@sfsu.edu>
2023-06-07 23:07:12 -0700

    1

--- calculator/src/main/java/edu/csc413/calculator/operators/MultiplyOperator.java
+++ calculator/src/main/java/edu/csc413/calculator/operators/MultiplyOperator.java
@@ -1,4 +1,4 @@
 package edu.csc413.calculator.operators;
-
+import  edu.csc413.calculator.evaluator.Operand;
 public class MultiplyOperator {
 }
--- calculator/src/main/java/edu/csc413/calculator/operators/SubtractionOperator.java
+++ calculator/src/main/java/edu/csc413/calculator/operators/SubtractionOperator.java
@@ -1,4 +1,5 @@
 package edu.csc413.calculator.operators;
+import edu.csc413.calculator.evaluator.Operand;
 
 public class SubtractionOperator {
 }
