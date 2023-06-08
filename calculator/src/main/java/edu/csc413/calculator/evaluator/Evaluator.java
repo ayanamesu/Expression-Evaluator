60cdf47db4718b0ff42fc7c77d6ed8a7a9c6cb83
ayanamesu <wlee17@sfsu.edu>
2023-06-08 00:31:29 -0700

    1

--- calculator/src/main/java/edu/csc413/calculator/evaluator/Evaluator.java
+++ calculator/src/main/java/edu/csc413/calculator/evaluator/Evaluator.java
@@ -17,12 +17,12 @@ public class Evaluator {
     operatorStack = new Stack<>();
   }
 
-  public int evaluateExpression(String expression ) throws InvalidTokenException {
+  public int evaluateExpression(String expression) throws InvalidTokenException {
     String expressionToken;
 
     // The 3rd argument is true to indicate that the delimiters should be used
     // as tokens, too. But, we'll need to remember to filter out spaces.
-    this.expressionTokenizer = new StringTokenizer( expression, this.delimiters, true );
+    this.expressionTokenizer = new StringTokenizer(expression, this.delimiters, true);
 
     // initialize operator stack - necessary with operator priority schema
     // the priority of any operator in the operator stack other than
@@ -30,15 +30,14 @@ public class Evaluator {
     // of the usual operators
 
 
-
-    while ( this.expressionTokenizer.hasMoreTokens() ) {
+    while (this.expressionTokenizer.hasMoreTokens()) {
       // filter out spaces
-      if ( !( expressionToken = this.expressionTokenizer.nextToken() ).equals( " " )) {
+      if (!(expressionToken = this.expressionTokenizer.nextToken()).equals(" ")) {
         // check if token is an operand
-        if ( Operand.check( expressionToken )) {
-          operandStack.push( new Operand( expressionToken ));
+        if (Operand.check(expressionToken)) {
+          operandStack.push(new Operand(expressionToken));
         } else {
-          if ( ! Operator.check( expressionToken )) {
+          if (!Operator.check(expressionToken)) {
             throw new InvalidTokenException(expressionToken);
           }
 
@@ -49,7 +48,7 @@ public class Evaluator {
           // skeleton for an example.
           Operator newOperator = Operator.getOperator(expressionToken);
 
-          while (operatorStack.peek().priority() >= newOperator.priority() ) {
+          while (operatorStack.peek().priority() >= newOperator.priority()) {
             // note that when we eval the expression 1 - 2 we will
             // push the 1 then the 2 and then do the subtraction operation
             // This means that the first number to be popped is the
@@ -57,16 +56,16 @@ public class Evaluator {
             Operator operatorFromStack = operatorStack.pop();
             Operand operandTwo = operandStack.pop();
             Operand operandOne = operandStack.pop();
-            Operand result = operatorFromStack.execute( operandOne, operandTwo );
-            operandStack.push( result );
+            Operand result = operatorFromStack.execute(operandOne, operandTwo);
+            operandStack.push(result);
           }
 
-          operatorStack.push( newOperator );
+          operatorStack.push(newOperator);
         }
       }
     }
 
-    while (operatorStack.peek().priority() > 0 ) {
+    while (operatorStack.peek().priority() > 0) {
       //process();
 
       Operator operatorCur = operatorStack.pop();
@@ -75,15 +74,18 @@ public class Evaluator {
       Operand result = operatorCur.execute(operandOne, operandTwo);
       operandStack.push(result);
 
-    // Control gets here when we've picked up all of the tokens; you must add
-    // code to complete the evaluation - consider how the code given here
-    // will evaluate the expression 1+2*3
-    // When we have no more tokens to scan, the operand stack will contain 1 2
-    // and the operator stack will have + * with 2 and * on the top;
-    // In order to complete the evaluation we must empty the stacks,
-    // that is, we should keep evaluating the operator stack until it is empty;
-    // Suggestion: create a method that processes the operator stack until empty.
+      // Control gets here when we've picked up all of the tokens; you must add
+      // code to complete the evaluation - consider how the code given here
+      // will evaluate the expression 1+2*3
+      // When we have no more tokens to scan, the operand stack will contain 1 2
+      // and the operator stack will have + * with 2 and * on the top;
+      // In order to complete the evaluation we must empty the stacks,
+      // that is, we should keep evaluating the operator stack until it is empty;
+      // Suggestion: create a method that processes the operator stack until empty.
 
+
+    }
     return operandStack.pop().getValue();
   }
 }
+
